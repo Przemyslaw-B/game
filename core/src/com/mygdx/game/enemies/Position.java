f5ed4b750b18e57f3481d5223f99a0a065ed33e0
Przemek <przemyslaw_baczek@o2.pl>
2023-08-19 22:03:56 +0200

    comit

---
+++ core/src/com/mygdx/game/enemies/Position.java
@@ -0,0 +1,14 @@
+package com.mygdx.game.enemies;
+
+import java.util.random.*;
+
+public class Position {
+
+    private int x;
+    private int y;
+
+    public Position(int x, int y){
+        this.x = x;
+        this.y=y;
+    }
+}
