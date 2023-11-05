fa28b11ab7e56ac75d73630f5270cfb8146d9371
Przemek <przemyslaw_baczek@o2.pl>
2023-11-05 16:46:33 +0100

    commit

--- core/src/com/mygdx/game/player/Movement.java
+++ core/src/com/mygdx/game/player/Movement.java
@@ -22,26 +22,34 @@ public class Movement extends Position{
     private void calculateOptimalVector(int inputX, int inputY){
         int oldX = vector[0];
         int oldY = vector[1];
-        if(inputX != 0 && inputY != 0){
-            float repeatX = inputX/getSpeedSideways();
-            float repeatY = inputY/getSpeedForward();
-            System.out.println("Numbers of REPEAT: X: " + repeatX + " Y: " + repeatY);
+        float repeatX = vector[0]/getSpeedSideways();
+        float repeatY = vector[1]/getSpeedForward();
+
+        if(repeatX < 0 ){
+            repeatX = -repeatX;
+        }
+        if(repeatY < 0){
+            repeatY = -repeatY;
+        }
+
+        if(repeatX != 0 && repeatY != 0){
             if(repeatY < repeatX){
                 vector[0] = getSpeedSideways();
-                vector[1] = (int) ((float) inputY/repeatX);
+                vector[1] = (int) ((float) getSpeedForward()/(repeatX/repeatY));
 
             } else if(repeatY == repeatX){
                 vector[0] = getSpeedSideways();
                 vector[1] = getSpeedForward();
             } else {
-                vector[0] = (int) ((float) inputX/repeatY);
+                vector[0] = (int) ((float) getSpeedSideways()/(repeatY/repeatX));
                 vector[1] = getSpeedForward();
             }
         } else {
-            if(inputX == 0 && inputY ==0){
+            if(repeatX == 0 && repeatY ==0){
                 vector[0] = 0;
                 vector[1] = 0;
-          } else if(inputY ==  0 && inputX != 0){
+          } else if(repeatY ==  0 && repeatX != 0){
+
                 vector[0] = getSpeedSideways();
                 vector[1] = 0;
             } else {
@@ -62,7 +70,6 @@ public class Movement extends Position{
     }
 
 
-
     private int getVerticalMove(int inputY){
         int vertical = inputY - getShipPositionY();
         return vertical;
@@ -73,118 +80,19 @@ public class Movement extends Position{
         return horizontal;
     }
 
-    private void moveVertical(int inputY){
-        int differenceY = inputY - getShipPositionY();
-        if(differenceY > 0){
-            moveForward(inputY, differenceY);    //move forward
-        } else if( differenceY < 0){
-            moveBackward(inputY, differenceY);   //move backward
-        } else {} //do not move vertical
-
-    }
-
-    private void moveHorizontal(int inputX){
-        int differenceX = inputX - getShipPositionX();
-        if(differenceX > 0){
-            moveRight(inputX, differenceX);
-        } else if(differenceX < 0){
-            moveLeft(inputX, differenceX);
-        } else {} //do not move horizontal
-    }
-
-    private void moveForward(int inputY, int differenceY){
-        int positionY = getShipPositionY();   //get current ship position
-        if(differenceY < getSpeedForward()){
-            positionY = inputY;
-        } else {
-            positionY += getSpeedForward();     //change position
-        }
-        setShipPositionY(positionY);          //save new position
-    }
-
-    private void moveBackward(int inputY, int differenceY){
-        int positionY= getShipPositionY(); //get current ship position
-        if(-differenceY < getSpeedForward()){
-            positionY = inputY;
-        } else {
-            positionY -= getSpeedForward(); //change position
-        }
-        setShipPositionY(positionY);      //save new position
-    }
-
-    private void moveRight(int inputX, int differenceX){
-        int positionX = getShipPositionX();   //Read current position of player ship
-        if(differenceX < getSpeedSideways()){
-            positionX = inputX;
-        } else {
-            positionX += getSpeedSideways();    //change position
-        }
-        setShipPositionX(positionX);          //save position
-    }
-
-    private void moveLeft(int inputX, int differenceX){
-        int positionX = getShipPositionX();   //Read current position of player ship
-        if(-differenceX < getSpeedSideways()){
-            positionX = inputX;
-        } else {
-            positionX -= getSpeedSideways();    //change position
-        }
-        setShipPositionX(positionX);          //save position
-    }
 
     private void moveVertical(){
-        if((vector[1]>0 && vector[1] <= getSpeedForward()) || (vector[1] <0 && -vector[1] <= getSpeedForward())){
-            /*
-            if(vector[1] > 0){
-                setShipPositionY(getShipPositionY() + vector[1]);
-            } else if (vector[1] < 0){
-                setShipPositionY(getShipPositionY() - vector[1]);
-            }
-            */
-        }
-        System.out.println("Old Y position: " + getShipPositionY());
         setShipPositionY(getShipPositionY() + vector[1]);
-        System.out.println("New Y position: " + getShipPositionY());
-
-
     }
 
-    private void moveHorizontal(){
-        if((vector[0]>0 && vector[0] <= getSpeedSideways()) || (vector[0] <0 && -vector[0] <= getSpeedSideways())){
-            /*
-            setShipPositionX(getShipPositionX() + vector[0]);
-            if(vector[0] > 0){
-                setShipPositionX(getShipPositionX() + vector[0]);
-            } else if (vector[0] < 0){
-                setShipPositionX(getShipPositionX() - vector[0]);
-            }
-            */
-        }
-        System.out.println("Old X position: " + getShipPositionX());
+    private void moveHorizontal() {
         setShipPositionX(getShipPositionX() + vector[0]);
-        System.out.println("New X position: " + getShipPositionX());
-
-
     }
 
     public void shipControl(){
         readInputAndSetVector();
-        System.out.println("Vector: X: " + vector[0] + " Y: " + vector[1]);
-
-        int inputX = Gdx.input.getX();
-        int inputY = Gdx.input.getY();
-        inputY = Gdx.app.getGraphics().getHeight() - inputY;    //odwróć oś
         moveVertical();
         moveHorizontal();
-
-        /*
-        int inputX = Gdx.input.getX();
-        int inputY = Gdx.input.getY();
-        inputY = Gdx.app.getGraphics().getHeight() - inputY;    //odwróć oś
-        moveHorizontal(inputX);
-        moveVertical(inputY);
-
-         */
     }
 
 }
