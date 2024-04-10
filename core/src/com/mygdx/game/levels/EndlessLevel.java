8021a34c540f02a252c85bcafce5191b039f7bf6
przemek <przemyslaw_baczek@o2.pl>
2024-04-10 22:28:00 +0200

    commit

--- core/src/com/mygdx/game/levels/EndlessLevel.java
+++ core/src/com/mygdx/game/levels/EndlessLevel.java
@@ -33,6 +33,7 @@ public class EndlessLevel implements levelInterface {
         this.width = Gdx.graphics.getWidth();
         this.height = Gdx.graphics.getHeight();
         Data.setDefault.resetEnemyToDefault();
+        this.enemyQueue = new ArrayList();
     }
 
     private int getRng(int min, int max){
@@ -62,7 +63,109 @@ public class EndlessLevel implements levelInterface {
 
     private void chooseEnemyToSpawn(int amount){
         this.amountSameEnemy = amount;
-        //TODO add new enemy to queue spawn;
+        int id = rollID();
+        boolean isFromTop = rollIsFromTop();
+        boolean isLeft = rollIsLeft();
+        int rotation = rollRotation(isFromTop, isLeft);
+        boolean isAimedOnPlayer = rollIsAimedOnPlayer(isFromTop);
+        int x = width;
+        int y = height;
+        if(!isFromTop){ //Same starting position for all enemies in cycle spawned on diagonal
+            x = rollPositionX(isFromTop, isLeft);
+            y = rollPositionY(x, isFromTop);
+        }
+        while(amountSameEnemy > 0){
+            if(isFromTop){  //Different position for enemies in cycle spawned NOT on diagonal
+                x = rollPositionX(isFromTop, isLeft);
+                y = rollPositionY(x, isFromTop);
+            }
+            Enemy tempEnemy = new Enemy(x, y, id, rotation, isFromTop, isAimedOnPlayer);
+            amountSameEnemy--;
+        }
+    }
+
+    private int rollID(){
+        int id = 2;
+        int maxRoll = 2;
+        id = getRng(2,maxRoll);
+        return id;
+    }
+
+    private boolean rollIsFromTop(){
+        int roll = getRng(0, 9);
+        if(roll >= 0 && roll <= 7){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean rollIsAimedOnPlayer(boolean isFromTop){
+        if(isFromTop){
+            return false;
+        }
+        int roll = getRng(0, 9);
+        if(roll >= 0 && roll <= 6){
+            return false;
+        }
+        return true;
+    }
+
+    private int rollPositionX(boolean isFromTop, boolean isLeft){
+        int x;
+        if(isFromTop){
+            int roll = getRng(0,2);
+            if(roll==1){ //from top left;
+                x = -50;
+                return x;
+            } else if(roll == 2){
+                x = getRng(0, width);
+                return x;
+            } else {
+                x = width + 50;
+                return x;
+            }
+        }
+        if(isLeft){
+            x = -50;
+            return x;
+        }
+        x = width + 50;
+        return x;
+    }
+
+    private int rollPositionY(int positionX, boolean isFromTop){
+        int y;
+        if(isFromTop){
+            if(positionX < 0 || positionX > width){
+                y = getRng(height-50, height);
+                return y;
+            } else {
+                y = height + 50;
+                return y;
+            }
+        }
+        y = getRng((int)(height*(2/3)), height);
+        return y;
+    }
+
+    private boolean rollIsLeft(){
+        int roll = getRng(0,1);
+        if(roll == 0){
+            return true;
+        }
+        return false;
+    }
+
+    private int rollRotation(boolean isFromTop, boolean isLeft){
+        if(isFromTop){
+            return 180;
+        }
+        if(isLeft){
+            int roll = getRng(90+30, 90+60);
+            return roll;
+        }
+        int roll = getRng(180 + 30, 180 + 60);
+        return roll;
     }
 
 
