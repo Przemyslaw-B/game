a75589f779c37fef2de8f95bfaa9fa84b706daf3
Przemek <przemyslaw_baczek@o2.pl>
2024-05-05 12:57:30 +0200

    Texture loader

--- core/src/com/mygdx/game/TextureLoader/BacgroundLoader/StarLoader.java
+++ core/src/com/mygdx/game/TextureLoader/BacgroundLoader/StarLoader.java
@@ -1,4 +1,4 @@
-package com.mygdx.game.TextureLoader.BacgroundPlayer;
+package com.mygdx.game.TextureLoader.BacgroundLoader;
 
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
